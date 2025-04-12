1a773b8ca4e0e8407513595cd8df6b6172c81d2f
Ria Sehgal <ria291199@gmail.com>
2025-04-12 19:39:30 +0530

    Initial commit

---
+++ src/main/java/com/projects/api/userandactivity/UserandactivityApplication.java
@@ -0,0 +1,13 @@
+package com.projects.api.userandactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserandactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserandactivityApplication.class, args);
+	}
+
+}
